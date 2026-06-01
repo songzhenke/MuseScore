7c21c85cf49520f3afe90d17bf0973417a66bae7
songzhenk <songzhenke@hotmail.com>
2026-06-01 20:32:11 +1000

    code review: Guard LayoutData before dereference in new draw paths

--- src/engraving/rendering/score/tdraw.cpp
+++ src/engraving/rendering/score/tdraw.cpp
@@ -1254,6 +1254,10 @@ void TDraw::draw(const DurationLine* item, Painter* painter, const PaintOptions&
     TRACE_DRAW_ITEM;
 
     const DurationLine::LayoutData* ldata = item->ldata();
+    IF_ASSERT_FAILED(ldata) {
+        return;
+    }
+
     painter->setPen(Pen(item->curColor(opt), ldata->lineWidth, PenStyle::SolidLine, PenCapStyle::FlatCap));
     painter->drawLine(LineF(0.0, 0.0, item->len(), 0.0));
 }
@@ -2478,7 +2482,12 @@ void TDraw::draw(const OctaveDot* item, Painter* painter, const PaintOptions& op
 {
     TRACE_DRAW_ITEM;
 
-    double rad = item->ldata()->radius;
+    const OctaveDot::LayoutData* ldata = item->ldata();
+    IF_ASSERT_FAILED(ldata) {
+        return;
+    }
+
+    double rad = ldata->radius;
     double x = item->len() * 0.5; // draw dot in the middle
 
     painter->setPen(Pen(item->curColor(opt)));
